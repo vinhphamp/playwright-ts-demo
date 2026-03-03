62d990e162a81bd271a53847ab3387f93318e6cc
vinhphamp <134673953+vinhphamp@users.noreply.github.com>
2026-03-03 08:30:02 +0000

    add new items of dashboardPage

--- pages/dashboardPage.ts
+++ pages/dashboardPage.ts
@@ -1,6 +1,36 @@
 import { Page, Locator, expect } from "@playwright/test";
 
-export class dashboardPage {
+export class DashboardPage {
+    readonly page: Page;
+    searchSidebar: Locator;
+    adminLink: Locator;
+    pimLink: Locator;
+
+
+
+    constructor(page: Page) {
+        this.page = page;
+        // this.searchSidebar = page.getByPlaceholder('Search');
+        this.searchSidebar = page.locator("//input[@placeholder='Search']");
+        this.adminLink = page.getByRole('link', {name: 'Admin'});
+        this.pimLink = page.locator("span:has-text('PIM')");
+        
+
+
+
+
+
+    }
+
+    async checkDashboardUI () {
+        // await expect(this.searchSidebar).toBeVisible({timeout: 10_000});
+        await expect(this.searchSidebar).toBeVisible();
+        await expect(this.adminLink).toBeVisible();
+        await expect(this.pimLink).toBeVisible();
+        console.log('Check UI completed!')
+
+
+    }  
     
 
 }
---
+++ tests/dashboard.spec.ts
@@ -0,0 +1,27 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '../pages/loginPage';
+import { DashboardPage } from '../pages/dashboardPage';
+import loginData from '../test-data/users/login.data.json';
+import envData from '../test-data/environment/urls.data.json';
+
+test.describe ('Dashboard Checking', () => {
+    let login: LoginPage;
+    let dashboard: DashboardPage;
+
+    test.beforeEach(async ({page}) => {
+        login = new LoginPage(page);
+        dashboard = new DashboardPage(page);
+        await login.goto(envData.test.url);
+        await login.login(loginData.validUser.username, loginData.validUser.password);
+        console.log("Load dashboard page successfully");
+
+    });
+
+    test('Verify the UI of dashboard page', async ({page}) => {
+        await dashboard.checkDashboardUI();
+
+
+    });
+
+});
+
