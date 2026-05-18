b481ba355a4fa8fce264bcce9fc76bc65acbba93
macbookpro <vinhfanfu@gmail.com>
2026-05-18 20:55:20 +0700

    update codes of iframe

--- tests/web/dragAndDropWithiFrame.spec.ts
+++ tests/web/dragAndDropWithiFrame.spec.ts
@@ -3,7 +3,8 @@ import { test, expect } from '@playwright/test';
 test('drag and drop with iFrame', async({page}) => {
     await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
     
-    const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
+    // const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
+    const frame = page.frameLocator(`div[rel-title="Photo Manager"] iframe[class='demo-frame']`)
     await frame.locator('li', { hasText: "High Tatras 2"}).dragTo(frame.locator('#trash'))
 
     //more precise control
