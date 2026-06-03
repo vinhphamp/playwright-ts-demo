050ebee06e4b028567250d0063c696c33f8a2a61
vinhphamp <vinhfanfu@gmail.com>
2026-06-03 17:22:00 +0700

    improve login page goto wait for ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      use: { ...devices['Desktop Chrome'] },
     },
 
     // {
